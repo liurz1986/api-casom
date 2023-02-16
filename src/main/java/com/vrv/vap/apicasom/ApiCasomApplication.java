1f1babb0839a30646ff5ff980a1918ed087128d7
wudi <wudi@vrvmail.com.cn>
2023-02-16 19:29:32 +0800

    修改加密相关内容

--- src/main/java/com/vrv/vap/apicasom/ApiCasomApplication.java
+++ src/main/java/com/vrv/vap/apicasom/ApiCasomApplication.java
@@ -2,10 +2,12 @@ package com.vrv.vap.apicasom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan({"com.vrv.vap.apicasom","com.vrv.vap.jpa.mapper","com.vrv.vap.common","com.vrv.vap.swagger2","com.vrv.vap.es","com.vrv.vap.jpa.req","com.vrv.vap.jpa.http","com.vrv.vap.syslog.*","com.vrv.vap.jpa.req"})
 public class ApiCasomApplication {
