6a8368fd2d8f12e49ab69a8367ec8559faa2b3e6
wudi <wudi@vrvmail.com.cn>
2023-02-16 13:58:05 +0800

    提交api-casom

---
+++ src/main/java/com/vrv/apicasom/ApiCasomApplication.java
@@ -0,0 +1,13 @@
+package com.vrv.apicasom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCasomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCasomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vrv/apicasom/ApiCasomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrv.apicasom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCasomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
