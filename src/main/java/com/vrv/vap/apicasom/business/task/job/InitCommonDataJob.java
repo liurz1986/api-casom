00a4bc26f0819ba04c6ac36fd6a68d432cd60a99
liurenzhao <liurenzhao123>
2023-06-29 16:25:33 +0800

    quart测试

--- src/main/java/com/vrv/vap/apicasom/business/task/job/InitCommonDataJob.java
+++ src/main/java/com/vrv/vap/apicasom/business/task/job/InitCommonDataJob.java
@@ -5,6 +5,10 @@ import com.vrv.vap.apicasom.business.task.service.HwMeetingService;
 import com.vrv.vap.apicasom.business.task.service.MeetingHttpService;
 import com.vrv.vap.apicasom.business.task.service.ZkyUnitService;
 import com.vrv.vap.apicasom.frameworks.util.RedisUtils;
+import com.vrv.vap.jpa.spring.SpringUtil;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +23,16 @@ import java.util.Map;
  * @since: 2023/2/17 15:12
  * @description: 初始化公共信息（token,分院/城市信息,会议室数量信息）
  */
-@Component
-@EnableScheduling
-@EnableAsync
-public class InitCommonDataJob {
+public class InitCommonDataJob implements Job {
     private static Logger logger = LoggerFactory.getLogger(InitCommonDataJob.class);
+
+    private MeetingHttpService meetingHttpService= SpringUtil.getBean(MeetingHttpService.class);
+    private HwMeetingService hwMeetingService= SpringUtil.getBean(HwMeetingService.class);
+    private ZkyUnitService zkyUnitService= SpringUtil.getBean(ZkyUnitService.class);
+    private RedisUtils redisUtils= SpringUtil.getBean(RedisUtils.class);
     public static String token = null;
-
-    @Autowired
-    private MeetingHttpService meetingHttpService;
-
-    @Autowired
-    private HwMeetingService hwMeetingService;
-
-    @Autowired
-    private ZkyUnitService zkyUnitService;
-
-    @Autowired
-    private RedisUtils redisUtils;
-
-    @Scheduled(cron = "${hw.meeting.token}")
-    public void getToken(){
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try{
             logger.info("初始化公共信息（token,分院/城市信息,会议室数量信息）任务执行");
             token = meetingHttpService.getToken(0);
@@ -47,16 +40,15 @@ public class InitCommonDataJob {
             updateCity();
             initMeetingRooms();
         }catch (Exception e){
-            logger.error("初始化公共信息（token,分院/城市信息,会议室数量信息）任务执行异常",e);
+            logger.error("初始化公共信息（token,分院/城市信息,会议室数量信息）任务执行异常:{}",e);
         }
 
     }
-
     /**
      * 更新城市信息
      */
     public void updateCity(){
-        Map<String, ZkyUnitBean> zkyUnitBeanMap = zkyUnitService.initCity();
+        Map<String,ZkyUnitBean> zkyUnitBeanMap = zkyUnitService.initCity();
         hwMeetingService.updateCity(zkyUnitBeanMap);
     }
 
@@ -67,5 +59,6 @@ public class InitCommonDataJob {
         int total = meetingHttpService.initMeetingRooms();
         redisUtils.set("MeetingRooms",total);
     }
-}
 
+
+}
--- src/main/java/com/vrv/vap/apicasom/business/task/job/ZkySendJob.java
+++ src/main/java/com/vrv/vap/apicasom/business/task/job/ZkySendJob.java
@@ -1,5 +1,10 @@
 package com.vrv.vap.apicasom.business.task.job;
 import com.vrv.vap.apicasom.business.task.service.*;
+import com.vrv.vap.jpa.spring.SpringUtil;
+import lombok.SneakyThrows;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +14,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
- * @author: 梁国露
- * @since: 2023/2/17 15:12
- * @description: 收发件消息
+ * 收发件任务
+ * 2023-06-20
+ * @author vrv
  */
- @Component
- @EnableScheduling
- @EnableAsync
-public class ZkySendJob {
-    // 日志
-    private Logger logger = LoggerFactory.getLogger(ZkySendJob.class);
-
-    @Autowired
-    private ZkySendDataService zkySendDataService;
-
-
-    @Scheduled(cron = "${hw.send.time}")
-    public void getZkySend(){
+public class ZkySendJob implements Job {
+    private static Logger logger = LoggerFactory.getLogger(ZkySendJob.class);
+    private ZkySendDataService zkySendDataService= SpringUtil.getBean(ZkySendDataService.class);
+    @SneakyThrows
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("定时执行收发件任务开始");
         try{
-            logger.info("定时执行收发件任务开始");
             zkySendDataService.excZkySend();
+            logger.info("定时执行收发件任务完成");
         }catch (Exception e){
-            logger.error("中科院文件信息同步失败：{}",e);
+            logger.error("定时执行收发件任务异常：{}",e);
         }
-    }
 
+
+    }
 }
